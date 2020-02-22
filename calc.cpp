be5e21ce54eed48fbdf8259c2d7ba6c4a483e11f
CtRl-V-B <58360359+CtRl-V-B@users.noreply.github.com>
2020-02-22 09:08:52 +0800

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,17 @@
+﻿
+#include "pch.h"
+#include <cstdio>
+#include <cstdlib>
+
+int main()
+{
+    system("title 计算器小程序 V-0.1 Test Version    by cyl");
+    work(); //封装好的函数
+    printf("计算器已成功退出！\n\n");
+    system("pause");
+    return 0;
+}
+
+
+
+//by Yunlang Cai 2020/2/21 calc v-0.1 test
